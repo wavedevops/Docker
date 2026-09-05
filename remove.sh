bfcc99465c3c453e1a298a9623908a5608d74251
Chowdary-Hari <chandrahari321@outlook.com>
2026-09-05 11:22:14 +0530

    automated commit #292

--- remove.sh
+++ remove.sh
@@ -1,4 +1,3 @@
-```bash
 #!/bin/bash
 
 # Colors
@@ -15,22 +14,19 @@ docker rm -f $(docker ps -aq) 2>/dev/null
 docker system prune -a --volumes -f 2>/dev/null
 docker volume prune -a -f 2>/dev/null
 
-echo
 echo -e "${GREEN}===== Containers =====${NC}"
 docker ps -a
 
-echo
+
 echo -e "${YELLOW}===== Images =====${NC}"
 docker images
 
-echo
+
 echo -e "${BLUE}===== Volumes =====${NC}"
 docker volume ls
 
-echo
 echo -e "${CYAN}===== Networks =====${NC}"
 docker network ls
 
-echo
 echo -e "${GREEN}===== Docker cleanup completed =====${NC}"
-```
+
